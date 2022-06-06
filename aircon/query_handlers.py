e638a1e18b713b422a0112239d790418920b9b49
SOLAREDGE\kfir.e <kfir.e@solaredge.com>
2022-06-07 00:21:58 +0300

    move to 3.10

--- aircon/query_handlers.py
+++ aircon/query_handlers.py
@@ -54,7 +54,8 @@ class QueryHandlers:
     device = self._devices_map[request.remote]
     command['seq_no'] = device.get_command_seq_no()
     try:
-      command['data'], property_updater = device.commands_queue.get_nowait()
+      command_entry = device.commands_queue.get_nowait()
+      command['data'], property_updater = command_entry.command, command_entry.updater
     except queue.Empty:
       command['data'], property_updater = {}, None
     if property_updater:
@@ -81,6 +82,9 @@ class QueryHandlers:
         logging.info('Unsupported update message = {}'.format(update['seq_no']))
         return response
       name = update['data']['name']
+      # Fix A/C typos.
+      if name == 'f_votage':
+        name = 'f_voltage'
       data_type = device.get_property_type(name)
       value = data_type(update['data']['value'])
       device.update_property(name, value)
